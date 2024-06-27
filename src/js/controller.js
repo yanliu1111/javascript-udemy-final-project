58b280feaae443407df5e21de3f74b06dbdff1fc
yanliu1111 <liuyan0504@gmail.com>
2024-06-27 11:20:55 -0600

    feat: build resultView

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,6 +5,7 @@ import * as model from './model.js';
 
 import { async } from 'regenerator-runtime';
 import recipeView from './views/recipeView.js';
+import resultsView from './views/resultsView.js';
 import searchView from './views/searchView.js';
 
 //core-js is for polyfilling modern JS features
@@ -13,7 +14,10 @@ import searchView from './views/searchView.js';
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
-
+// come from parcel
+if (module.hot) {
+  module.hot.accept();
+}
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
@@ -32,13 +36,15 @@ const controlRecipes = async function () {
 };
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // 1. Get search query
     const query = searchView.getQuery();
     if (!query) return;
     // 2. Load search results
     await model.loadSearchResults(query);
     // 3. Render results
-    console.log(model.state.search.results);
+    //console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
