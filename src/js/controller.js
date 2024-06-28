1c5608b870ae24d938d47710dd08105caab9a390
yanliu1111 <liuyan0504@gmail.com>
2024-06-28 00:59:39 -0600

    feat: DOM pagination button

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import 'regenerator-runtime/runtime';
 import * as model from './model.js';
 
 import { async } from 'regenerator-runtime';
+import paginationView from './views/paginationView.js';
 import recipeView from './views/recipeView.js';
 import resultsView from './views/resultsView.js';
 import searchView from './views/searchView.js';
@@ -46,7 +47,8 @@ const controlSearchResults = async function () {
     //console.log(model.state.search.results);
     // resultsView.render(model.state.search.results);
     resultsView.render(model.getSearchResultsPage());
-    // paginationView.render(model.state.search);
+    // 4. Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -7,7 +7,7 @@ export default class View {
       return this.renderError();
 
     this._data = data;
-    console.log(this._data);
+    // console.log(this._data);
     const markup = this._generateMarkup();
     this._clear();
     this._parentElement.innerHTML = ('afterbegin', markup);
