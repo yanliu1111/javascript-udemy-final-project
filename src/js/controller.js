269f257796222cfff48c35f677de23fb338ab476
yanliu1111 <liuyan0504@gmail.com>
2024-06-29 13:36:32 -0600

    feat: update dom algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -69,7 +69,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
   // Update the recipe view
   // recipeView.render(model.state.recipe);
-  recipeView.render(model.state.recipe);
+  //update function in View.js
+  recipeView.update(model.state.recipe);
 };
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
