cb9f9fc419e8c141a7d454685dce9dba85feb3fb
yanliu1111 <liuyan0504@gmail.com>
2024-06-30 12:38:50 -0600

    feat: clean

--- src/js/config.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const API_KEY = 'ec96034d-981f-4de7-ac81-0782eebc7004';
